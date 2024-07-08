5644f4e4d669c52611df8e8fc9266b1205ce1c49
Gesaias <gesaiasalvessouza@gmail.com>
2024-07-08 11:10:18 -0400

    feat: add project in nextapp

--- nextapp/cypress.config.ts
+++ nextapp/cypress.config.ts
@@ -3,12 +3,15 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    // baseUrl: 'https://erickwendel.github.io/vanilla-js-web-app-example',
     baseUrl: 'http://localhost:3000',
+    // não vai limpar o estado da tela após cada it
+    testIsolation: false,
   },
-
   component: {
     devServer: {
       framework: 'next',
--- nextapp/cypress/e2e/home/home.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('Home', () => {
-  it('should be navigate to the home page', () => {
-    cy.visit('/');
-  });
-});
